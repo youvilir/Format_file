a36aab04705667ef47d029a2aa32c06cb62f20e8
Павел Пугачев <Pugachev.P@edu.rea.ru>
2021-03-22 21:15:22 +0300

    Добавьте файлы проекта.

---
+++ Format_file/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Format_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
